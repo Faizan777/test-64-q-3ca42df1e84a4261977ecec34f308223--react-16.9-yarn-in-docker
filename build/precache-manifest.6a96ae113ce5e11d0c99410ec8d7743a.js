08294c07052dc57c2c7669a48939cfad57e2ce3a
Faizan777 <39923225+Faizan777@users.noreply.github.com>
2020-05-08 17:41:06 +0530

    Build Added

---
+++ build/precache-manifest.6a96ae113ce5e11d0c99410ec8d7743a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20dc8964459eac0f13f0369a5dc9069",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673eb3774f2e35b6b910",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "ae25ee8a5b34c79af79f",
+    "url": "/static/js/2.ec011850.chunk.js"
+  },
+  {
+    "revision": "673eb3774f2e35b6b910",
+    "url": "/static/js/main.3ed40d2d.chunk.js"
+  },
+  {
+    "revision": "6be36574329994164249",
+    "url": "/static/js/runtime~main.45577b49.js"
+  }
+]);
